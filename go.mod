09388daf4e57346de5c972c39e0868ca0a5feb5e
ViscousTorque <viscoustorque@yahoo.co.uk>
2024-12-08 13:57:05 +0000

    Updates after Udemy course section 22

--- go.mod
+++ go.mod
@@ -5,12 +5,14 @@ go 1.22.2
 require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-playground/validator/v10 v10.23.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang/mock v1.6.0
+	github.com/google/uuid v1.4.0
 	github.com/jackc/pgx/v5 v5.7.1
 	github.com/spf13/viper v1.19.0
 	github.com/stretchr/testify v1.10.0
 	github.com/techschool/simplebank v0.0.0-20240804143235-cc1ef6d0b5dd
-	golang.org/x/crypto v0.29.0
+	golang.org/x/crypto v0.30.0
 )
 
 require (
@@ -29,7 +31,6 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
-	github.com/google/uuid v1.4.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
---
+++ token/jwtToken.go
@@ -0,0 +1,72 @@
+package token
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+const minJWTSecretSize = 32
+
+type JWTToken struct {
+	secretKey string
+}
+
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) < minJWTSecretSize {
+		return nil, fmt.Errorf("invalid Key : too short")
+	}
+	return &JWTToken{secretKey}, nil
+}
+
+func (maker *JWTToken) CreateToken(username string, duration time.Duration) (string, error) {
+
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+	return jwtToken.SignedString([]byte(maker.secretKey))
+}
+
+func (maker *JWTToken) VerifyToken(token string) (*Payload, error) {
+
+	// To overcome the trivial forgery vulnerabilty
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(maker.secretKey), nil
+	}
+
+	/* Could be 2 different errors : invalid or expired
+	To differentiate the differences you need to look into the implementation of the library
+
+	// The error from Parse if token is not valid
+		type ValidationError struct {
+			Inner  error  // stores the error returned by external dependencies, i.e.: KeyFunc
+			Errors uint32 // bitfield.  see ValidationError... constants
+			text   string // errors that do not have a valid error just have text
+		}
+	*/
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
+}
