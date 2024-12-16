0ee207a6e65cdc79bd5a957a03231b2e1277d2fb
ViscousTorque <19613303+ViscousTorque@users.noreply.github.com>
2024-12-16 14:09:26 +0000

    Updates after Udemy course section 56 - redis task (#16)

---
+++ worker/processor.go
@@ -0,0 +1,53 @@
+package worker
+
+import (
+	"context"
+
+	db "simplebank/db/sqlc"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/hibiken/asynq"
+)
+
+const (
+	QueueCritical = "critical"
+	QueueDefault  = "default"
+)
+
+type TaskProcessor interface {
+	Start() error
+	Shutdown()
+	ProcessTaskSendVerifyEmail(ctx context.Context, task *asynq.Task) error
+}
+
+type RedisTaskProcessor struct {
+	server *asynq.Server
+	store  db.Store
+}
+
+func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	logger := NewLogger()
+	redis.SetLogger(logger)
+
+	server := asynq.NewServer(
+		redisOpt,
+		asynq.Config{}, // for now lets use the default settings
+	)
+
+	return &RedisTaskProcessor{
+		server: server,
+		store:  store,
+	}
+}
+
+func (processor *RedisTaskProcessor) Start() error {
+	mux := asynq.NewServeMux()
+
+	mux.HandleFunc(TaskSendVerifyEmail, processor.ProcessTaskSendVerifyEmail)
+
+	return processor.server.Start(mux)
+}
+
+func (processor *RedisTaskProcessor) Shutdown() {
+	processor.server.Shutdown()
+}
