86aaa8106984eebf0d18a6f4d7760815e153885f
ViscousTorque <viscoustorque@yahoo.co.uk>
2024-12-18 18:22:23 +0000

    Updates after Udemy course section 66 - unit tests grpc auth

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,14 +1,18 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	db "simplebank/db/sqlc"
+	"simplebank/token"
 	"simplebank/util"
 	"simplebank/worker"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
@@ -21,4 +25,18 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, _, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+
+	bearerToken := fmt.Sprintf("%s %s", authorisationBearer, accessToken)
+	md := metadata.MD{
+		authorisationHeader: []string{
+			bearerToken,
+		},
+	}
+
+	return metadata.NewIncomingContext(context.Background(), md)
 }
