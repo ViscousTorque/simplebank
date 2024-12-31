40a6e2ab12b5ffccd76ddc66440d7e9456ca2cfd
ViscousTorque <19613303+ViscousTorque@users.noreply.github.com>
2024-12-31 18:15:31 +0000

    Update after Frontend Crash Course section 2 (#36)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome to Simple Bank</h1>
   </main>
 </template>
