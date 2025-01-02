21b677e32f5f8c1339137164c3385d42b6b6e808
ViscousTorque <19613303+ViscousTorque@users.noreply.github.com>
2025-01-02 12:44:54 +0000

    Update after Frontend Crash Course section 3 (#37)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,24 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
 import App from './App.vue'
 import router from './router'
+import Aura from '@primevue/themes/aura';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(PrimeVue, {
+    // Default theme configuration
+    theme: {
+        preset: Aura,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false
+        }
+    }
+ });
 
 app.mount('#app')
