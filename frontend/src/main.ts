05b54da05af8bfa522e9206daec11d73c11c47f5
ViscousTorque <19613303+ViscousTorque@users.noreply.github.com>
2025-01-04 09:21:59 +0000

    Update after Frontend Crash Course section 5 - add toast (#41)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import Aura from '@primevue/themes/aura';
+import ToastService from 'primevue/toastservice';
 
 import App from './App.vue'
 import router from './router'
@@ -21,5 +22,6 @@ app.use(PrimeVue, {
         }
     }
  });
+ app.use(ToastService);
 
 app.mount('#app')
