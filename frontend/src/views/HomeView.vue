4364dff2b53dc301c7c768ecddbec506f49fb503
ViscousTorque <19613303+ViscousTorque@users.noreply.github.com>
2025-01-04 10:00:34 +0000

    Update after Frontend Crash Course section 6 - add card after login (#42)

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import Toast from 'primevue/toast';
+
 import LoginUser from '@/components/LoginUser.vue';
+import UserInfo from '@/components/UserInfo.vue'
+import store from '../store'
 </script>
 
 <template>
   <main>
     <Toast />
     <h1 class="green">Welcome to Simple Bank!</h1>
-    <LoginUser/>
+    <UserInfo v-if="store.state.user" :user="store.state.user" />
+    <LoginUser v-else />
   </main>
 </template>
