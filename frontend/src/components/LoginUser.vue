06358f1ac5776583f6668980f4e56b63bfab3bba
ViscousTorque <viscoustorque@yahoo.co.uk>
2025-01-04 09:18:52 +0000

    Update after Frontend Crash Course section 5 - add toast

--- frontend/src/components/LoginUser.vue
+++ frontend/src/components/LoginUser.vue
@@ -4,18 +4,66 @@ import InputGroupAddon from 'primevue/inputgroupaddon'
 import InputText from 'primevue/inputtext'
 import FloatLabel from 'primevue/floatlabel';
 import Button from 'primevue/button';
+import { useToast } from 'primevue/usetoast'
 import { ref } from 'vue';
 import axios from 'axios'
+import type { User } from '@/types/user'
+import store from '@/store'
+
+// TODO: add expiration date of the refresh token
+interface LoginResponse {
+  user: User
+  access_token: string
+  refresh_token: string
+}
 
 const username = ref<string>('')
 const password = ref<string>('')
+const errorMessage = ref<string>('')
+const toast = useToast()
+
 const handleLogin = async () => {
-  const response = await axios.post('http://localhost:8080/v1/login_user',
+  try {
+    const response = await axios.post<LoginResponse>(
+      'http://localhost:8080/v1/login_user',
       {
         username: username.value,
         password: password.value
-      },)
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'none'
+        }
+      }
+    )
+
+    store.setUser(response.data.user, response.data.access_token, response.data.refresh_token)
+    toast.add({
+      severity: 'success',
+      summary: `Hello, ${response.data.user.full_name}!`,
+      detail: `You have successfully logged in.`,
+      life: 3000
+    })
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error)) {
+      if (error.response && error.response.status === 404) {
+        errorMessage.value = error.response.data.message;
+      } else {
+        errorMessage.value = 'An error occurred. Please try again later';
+      }
+    } else {
+      errorMessage.value = 'An unexpected error occurred. Please try again.';
+    }
+
+    toast.add({
+      severity: 'error',
+      summary: 'Login failed',
+      detail: errorMessage.value,
+      life: 3000
+    })
   }
+}
 </script>
 
 <template>  
